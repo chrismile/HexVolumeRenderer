edc1b0195b04e0862bb5ccc33ef647311d4bf8a6
Christoph Neuhauser <c.a.neuhauser@gmail.com>
2020-01-23 14:19:11 +0100

    Initial commit with code structure.

---
+++ src/Renderers/HexahedralMeshRenderer.hpp
@@ -0,0 +1,54 @@
+/*
+ * BSD 2-Clause License
+ *
+ * Copyright (c) 2020, Christoph Neuhauser
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef RENDERERS_HEXAHEDRALMESHRENDERER_HPP
+#define RENDERERS_HEXAHEDRALMESHRENDERER_HPP
+
+class HexahedralMeshRenderer {
+public:
+    virtual ~HexahedralMeshRenderer() {}
+
+    // Returns if the visualization mapping needs to be re-generated.
+    inline bool isDirty() { return dirty; }
+    // Returns if the data needs to be re-rendered, but the visualization mapping is valid.
+    inline bool needsReRender() { bool tmp = reRender; reRender = false; return tmp; }
+
+    // Re-generates the visualization mapping.
+    virtual void generateVisualizationMapping(GeneralizedMapPtr meshIn)=0;
+
+    // Renders the object to the scene framebuffer.
+    virtual void render(const SceneData& sceneData)=0;
+    // Renders the GUI. The "dirty" and "reRender" flags might be set depending on the user's actions.
+    virtual void renderGui()=0;
+
+private:
+    bool dirty = true;
+    bool reRender = true;
+};
+
+#endif RENDERERS_HEXAHEDRALMESHRENDERER_HPP
