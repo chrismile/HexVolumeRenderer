e251e52d7a9e47abda863f45c52c611aa4db6b62
Christoph Neuhauser <c.a.neuhauser@gmail.com>
2020-03-22 22:05:43 +0100

    Added tube shader.

---
+++ Data/Shaders/TubeWireframe.glsl
@@ -0,0 +1,123 @@
+-- Vertex
+
+#version 430 core
+
+layout(location = 0) in vec3 vertexPosition;
+layout(location = 1) in vec3 vertexNormal;
+layout(location = 2) in vec3 vertexTangent;
+layout(location = 3) in vec4 vertexColor;
+
+out vec3 fragmentPositionWorld;
+out vec3 fragmentNormal;
+out vec3 fragmentTangent;
+out vec4 fragmentColor;
+
+void main()
+{
+    fragmentPositionWorld = (mMatrix * vec4(vertexPosition, 1.0)).xyz;
+    fragmentNormal = vertexNormal;
+    fragmentTangent = vertexTangent;
+    fragmentColor = vertexColor;
+    gl_Position = mvpMatrix * vec4(vertexPosition, 1.0);
+}
+
+
+-- Fragment
+
+#version 430 core
+
+in vec3 fragmentPositionWorld;
+in vec3 fragmentNormal;
+in vec3 fragmentTangent;
+in vec4 fragmentColor;
+
+#if defined(DIRECT_BLIT_GATHER)
+out vec4 fragColor;
+#endif
+
+uniform vec3 cameraPosition;
+
+/*
+uniform float minCriterionValue = 0.0;
+uniform float maxCriterionValue = 1.0;
+
+// Transfer function color lookup table
+uniform sampler1D transferFunctionTexture;
+
+vec4 transferFunction(float attr) {
+    // Transfer to range [0,1]
+    float posFloat = clamp((attr - minCriterionValue) / (maxCriterionValue - minCriterionValue), 0.0, 1.0);
+    // Look up the color value
+    return texture(transferFunctionTexture, posFloat);
+}
+*/
+
+#include "Lighting.glsl"
+
+#if !defined(DIRECT_BLIT_GATHER)
+#include OIT_GATHER_HEADER
+#endif
+
+void main()
+{
+    //vec4 color = transferFunction(fragmentAttribute);
+    vec4 color = fragmentColor;
+    color = blinnPhongShadingHalo(color);
+
+    #if defined(DIRECT_BLIT_GATHER)
+    fragColor = color;
+    #else
+    gatherFragmentCustomDepth(color, fragmentDepth);
+    #endif
+}
+
+
+-- Fragment.ClearView.Focus
+
+#version 430 core
+
+in vec3 fragmentPositionWorld;
+in vec3 fragmentNormal;
+in vec3 fragmentTangent;
+in vec4 fragmentColor;
+
+#if defined(DIRECT_BLIT_GATHER)
+out vec4 fragColor;
+#endif
+
+/*
+uniform float minCriterionValue = 0.0;
+uniform float maxCriterionValue = 1.0;
+
+// Transfer function color lookup table
+uniform sampler1D transferFunctionTexture;
+
+vec4 transferFunction(float attr) {
+    // Transfer to range [0,1]
+    float posFloat = clamp((attr - minCriterionValue) / (maxCriterionValue - minCriterionValue), 0.0, 1.0);
+    // Look up the color value
+    return texture(transferFunctionTexture, posFloat);
+}
+*/
+
+#include "ClearView.glsl"
+#include "Lighting.glsl"
+
+#if !defined(DIRECT_BLIT_GATHER)
+#include OIT_GATHER_HEADER
+#endif
+
+void main()
+{
+    //vec4 color = transferFunction(fragmentAttribute);
+    vec4 color = fragmentColor;
+    color = blinnPhongShadingHalo(color);
+    color.a *= getClearViewFocusFragmentOpacityFactor();
+
+    #if defined(DIRECT_BLIT_GATHER)
+    fragColor = color;
+    #else
+    gatherFragmentCustomDepth(color, fragmentDepth);
+    #endif
+}
+
--- Data/Shaders/Utils/Lighting.glsl
+++ Data/Shaders/Utils/Lighting.glsl
@@ -1,9 +1,12 @@
-// Simplified Blinn-Phong Shading assuming the ambient and diffuse color are equal and the specular color is white.
-// Assumes the following global variables are given: cameraPosition, fragmentPositionWorld, fragmentNormal.
+/**
+ * Simplified Blinn-Phong shading assuming the ambient and diffuse color are equal and the specular color is white.
+ * Assumes the following global variables are given: cameraPosition, fragmentPositionWorld, fragmentNormal.
+ * The camera position is assumed to be the source of a point light.
+*/
 vec4 blinnPhongShading(in vec4 baseColor) {
     // Blinn-Phong Shading
     const vec3 lightColor = vec3(1,1,1);
-    const vec3 ambientColor = baseColor.rgb;//vec3(0.5, fragmentPositionWorld.g, fragmentPositionWorld.b);
+    const vec3 ambientColor = baseColor.rgb;
     const vec3 diffuseColor = ambientColor;
     vec3 phongColor = vec3(0);
 
@@ -22,6 +25,47 @@ vec4 blinnPhongShading(in vec4 baseColor) {
     vec3 Is = kS * pow(clamp(abs(dot(n, h)), 0.0, 1.0), s) * lightColor;
 
     phongColor = Ia + Id + Is;
+
+    vec4 color = vec4(phongColor, baseColor.a);
+    return color;
+}
+
+
+/**
+ * Simplified Blinn-Phong shading for tubes assuming the ambient and diffuse color are equal and the specular color is
+ * white. It adds a halo at the outline of the tube. Assumes the following global variables are given:
+ * cameraPosition, fragmentPositionWorld, fragmentNormal, fragmentTangent.
+ * The camera position is assumed to be the source of a point light.
+*/
+vec4 blinnPhongShadingHalo(in vec4 baseColor) {
+    // Blinn-Phong Shading
+    const vec3 lightColor = vec3(1,1,1);
+    const vec3 ambientColor = baseColor.rgb;
+    const vec3 diffuseColor = ambientColor;
+    vec3 phongColor = vec3(0);
+
+    const float kA = 0.2;
+    const vec3 Ia = kA * ambientColor;
+    const float kD = 0.7;
+    const float kS = 0.1;
+    const float s = 10;
+
+    const vec3 n = normalize(fragmentNormal);
+    const vec3 t = normalize(fragmentTangent);
+    const vec3 v = normalize(cameraPosition - fragmentPositionWorld);
+    const vec3 l = v;//normalize(lightDirection);
+    const vec3 h = normalize(v + l);
+
+    vec3 Id = kD * clamp(abs(dot(n, l)), 0.0, 1.0) * diffuseColor;
+    vec3 Is = kS * pow(clamp(abs(dot(n, h)), 0.0, 1.0), s) * lightColor;
+
+    float haloParameter = 1.0;
+    float angle1 = abs(dot(v, n));
+    float angle2 = abs(dot(v, normalize(t))) * 0.7;
+    float halo = clamp(mix(1.0, angle1 + angle2, haloParameter), 0.0, 1.0);
+
+    phongColor = Ia + Id + Is;
+    phongColor *= halo;
 
     vec4 color = vec4(phongColor, baseColor.a);
     return color;
