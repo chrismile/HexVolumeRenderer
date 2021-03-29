df595a46bb0bcffaeb317347e5d79b46be397793
Christoph Neuhauser <c.a.neuhauser@gmail.com>
2021-03-29 18:51:30 +0200

    Updated date.

--- src/Widgets/ReplayWidget.cpp
+++ src/Widgets/ReplayWidget.cpp
@@ -1,7 +1,7 @@
 /*
  * BSD 2-Clause License
  *
- * Copyright (c) 2020, Christoph Neuhauser
+ * Copyright (c) 2020-2021, Christoph Neuhauser
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
