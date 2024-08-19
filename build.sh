019456566fb4a5af8b1ef18cacccb15517a9cb05
Christoph Neuhauser <c.a.neuhauser@gmail.com>
2024-08-19 17:45:31 +0200

    Removed incorrectly replaced variable in build script.

--- build.sh
+++ build.sh
@@ -1010,7 +1010,6 @@ elif [ $use_macos = true ] && [ $use_vcpkg = false ]; then
             codesign --force -s - "$filename" &> /dev/null
         fi
     done
-${copy_dependencies_macos_post}
 else
     mkdir -p $destination_dir/bin
 
