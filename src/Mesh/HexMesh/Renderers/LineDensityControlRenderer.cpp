9cc3352f04480e417f9256b4596ec7995b2b9d9c
Christoph Neuhauser <c.a.neuhauser@gmail.com>
2024-08-30 12:26:22 +0200

    Improved compatibility with C++20.

--- src/Mesh/HexMesh/Renderers/LineDensityControlRenderer.cpp
+++ src/Mesh/HexMesh/Renderers/LineDensityControlRenderer.cpp
@@ -27,6 +27,7 @@
  */
 
 #include <Math/Geometry/MatrixUtil.hpp>
+#include <Utils/StringUtils.hpp>
 #include <Utils/File/Logfile.hpp>
 #include <Utils/AppSettings.hpp>
 #include <Graphics/Renderer.hpp>
@@ -340,19 +341,19 @@ void LineDensityControlRenderer::renderGui() {
         if (ImGui::SliderFloat("Line Width", &lineWidth, MIN_LINE_WIDTH, MAX_LINE_WIDTH, "%.4f")) {
             reRender = true;
         }
-        if (ImGui::SliderFloat(u8"\u03BB", &lambda, 0.0f, 10.0f, "%.4f")) {
+        if (ImGui::SliderFloat(U8("\u03BB"), &lambda, 0.0f, 10.0f, "%.4f")) {
             reRender = true;
         }
-        if (ImGui::SliderFloat(u8"m", &factor_m, 0.0f, 50.0f, "%.4f")) {
+        if (ImGui::SliderFloat(U8("m"), &factor_m, 0.0f, 50.0f, "%.4f")) {
             reRender = true;
         }
-        if (ImGui::SliderFloat(u8"c", &factor_c, 0.0f, 50.0f, "%.4f")) {
+        if (ImGui::SliderFloat(U8("c"), &factor_c, 0.0f, 50.0f, "%.4f")) {
             reRender = true;
         }
-        if (ImGui::SliderFloat(u8"v", &factor_v, 0.0f, 50.0f, "%.4f")) {
+        if (ImGui::SliderFloat(U8("v"), &factor_v, 0.0f, 50.0f, "%.4f")) {
             reRender = true;
         }
-        if (ImGui::SliderFloat(u8"d", &factor_d, 0.0f, 50.0f, "%.4f")) {
+        if (ImGui::SliderFloat(U8("d"), &factor_d, 0.0f, 50.0f, "%.4f")) {
             reRender = true;
         }
     }
